bcc46bbd67ff4d87d116e3736896459d70e66db4
TheDG <dsinay@uc.cl>
2019-12-23 00:05:20 -0800

    Kernel SVM Classification

--- Machine_Learning_A-Z_Mine/Part 3 - Classification/Section 17 - Kernel SVM/kernel_svm.py
+++ Machine_Learning_A-Z_Mine/Part 3 - Classification/Section 17 - Kernel SVM/kernel_svm.py
@@ -1,7 +1,8 @@
-# Support Vector Machine
+# Kernel SVM
 
 # %% codecell
 # Importing the libraries
+import data_preprocessing_template as preprocessed_data
 import sys
 import os
 from sklearn.svm import SVC
@@ -10,8 +11,8 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import numpy as np
 if '__file__' not in globals():
-    sys.path.append(os.getcwd() + '/Machine_Learning_A-Z_Mine/Part 3 - Classification/Section 16 - Support Vector Machine (SVM)')
-import data_preprocessing_template as preprocessed_data
+    sys.path.append(os.getcwd(
+    ) + '/Machine_Learning_A-Z_Mine/Part 3 - Classification/Section 17 - Kernel SVM')
 
 
 # %% codecell
@@ -20,8 +21,8 @@ x_train, x_test, y_train, y_test, sc_x = preprocessed_data.preprocess_data()
 
 
 # %% codecell
-# Fitting SVM to the Training set
-classifier = SVC(kernel="linear", random_state= 0)
+# Fitting Kernel SVM to the Training set
+classifier = SVC(kernel="rbf", random_state=0)
 classifier.fit(x_train, y_train)
 
 
@@ -33,21 +34,21 @@ y_pred = classifier.predict(x_test)
 # %% codecell
 # Making the Confusion Matrix
 cm = confusion_matrix(y_test, y_pred)
-cm
+
 
 # %% codecell
 # Visualising the Training set results
 x_set, y_set = x_train, y_train
-X1, X2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+X1, X2 = np.meshgrid(np.arange(start=x_set[:, 0].min() - 1, stop=x_set[:, 0].max() + 1, step=0.01),
+                     np.arange(start=x_set[:, 1].min() - 1, stop=x_set[:, 1].max() + 1, step=0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+             alpha=0.75, cmap=ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('SVM (Training set)')
+                c=ListedColormap(('red', 'green'))(i), label=j)
+plt.title('Kernel SVM (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -56,16 +57,16 @@ plt.show()
 # %% codecell
 # Visualising the Test set results
 X_set, y_set = x_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+             alpha=0.75, cmap=ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('SVM (Test set)')
+                c=ListedColormap(('red', 'green'))(i), label=j)
+plt.title('Kernel SVM (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
